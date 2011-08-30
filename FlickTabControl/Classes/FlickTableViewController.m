e50b737c2d8af0b67fdaeb973c53f87e4a87c5f4
jk <jens.kohl@gmail.com>
2011-08-30 16:53:15 +0200

    initWithFrame was refactored to initWithStyle due deprecation warning

--- FlickTabControl/Classes/FlickTableViewController.m
+++ FlickTabControl/Classes/FlickTableViewController.m
@@ -103,7 +103,7 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
     // Set up the cell...
